8a4fd3b052b03f8d721840830e36993f3877904b
thisfo <this.fo.mail@gmail.com>
2018-05-06 18:47:03 +0200

    Added api with serializer

--- config/settings/base.py
+++ config/settings/base.py
@@ -45,11 +45,11 @@ DJANGO_APPS = (
 )
 
 THIRD_PARTY_APPS = (
-
+    'rest_framework',
 )
 
 LOCAL_APPS = (
-
+    'project.api',
 )
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
@@ -147,3 +147,7 @@ STATICFILES_FINDERS = (
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = str(APPS_DIR('media'))
+
+REST_FRAMEWORK = {
+
+}
